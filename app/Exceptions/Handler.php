aec834fb8a52111deccef817ea3cbe643c5090b2
Adedapo Emmanuel <thunadex1@gmail.com>
2022-03-22 22:11:18 +0100

    refactor: exception/handler.php to use the json responder trait

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Traits\JsonExceptionHandlerTrait;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+
+    use JsonExceptionHandlerTrait;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -36,6 +45,37 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e) {
+            return $this->routeNotFound();
+        });
+
+        $this->renderable(function (AuthenticationException $e) {
+            return $this->unauthenticatedError($e);
+        });
+
+        $this->renderable(function (RequestException $e) {
+            return $this->httpClientError($e);
+        });
+
+        $this->renderable(function (AuthorizationException $e) {
+            return $this->authorizationError($e);
+        });
+
+
+        $this->renderable(function (RequestException $e) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with('message', [
+                    'status' => 'error',
+                    'body'   => 'Something went wrong. Please try again',
+                ]);
+        });
+
+        $this->renderable(function (TokenMismatchException $e) {
+            return redirect()->back();
         });
     }
 }
