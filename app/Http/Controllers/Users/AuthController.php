ae9bf07aa95993b9733f9d9c25bf2030dd7b5d7d
Adedapo Emmanuel <thunadex1@gmail.com>
2022-03-22 22:33:04 +0100

    chore: added new model verification code

--- app/Http/Controllers/Users/AuthController.php
+++ app/Http/Controllers/Users/AuthController.php
@@ -5,10 +5,123 @@ namespace App\Http\Controllers\Users;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Traits\ReferenceGeneratorTrait;
 
 class AuthController extends ApiController
 {
-    public function register(){
-        return $this->respondWithSuccess("Hey, You are Good", 200);
+    public function register(Request $request){
+        try{
+            //Validate the user input
+            $validator = $this->validateUserRegistration();
+
+            if($validator->fails()){
+                return $this->respondWithValidationError($validator->messages(), 422);
+            }
+
+            DB::beginTransaction();
+
+            //Create a User
+            $createUser  = User::create([
+                'uid' => $this->uniqueUid(),
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $createUserUsedPassword = UserUsedPassword::create([
+                'user_id' => $createUser->id,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $createProfile = UserProfile::create([
+                'uid' => $this->uniqueUid(),
+                'user_id' => $createUser->id
+            ]);
+
+            //Generate and register email verification code
+            $generatedVerificationCode = VerificationCode::create([
+                'user_id' => $createUser->id,
+                'code' => mt_rand(100000,999999),
+                'purpose' => 'email_verification',
+                'expires_at' => Carbon::now()->addMinutes(15),
+            ]);
+
+            if($createUser && $createUserUsedPassword && $createProfile && $generatedVerificationCode){
+                DB::commit();
+
+                event(new UserRegistered($createUser, $generatedVerificationCode->code));
+
+                $data = [
+                    'token' => $createUser->createToken('UserAuthToken')->plainTextToken,
+                    'token_type' => "Bearer"
+                ];
+
+                return $this->respondWithSuccess("User Account Registered Successfully", 200, $data);
+            }else {
+                DB::rollBack();
+                return $this->respondWithError("Something went wrong", 503);
+            }
+        }
+        catch (\Exception $e){
+            return $this->exceptionError($e->getMessage(), 500);
+        }
+    }
+
+
+    public function login(Request $request){
+        try {
+            //Validate the user input
+            $validator = $this->validateUserLogin();
+
+            if($validator->fails()){
+                return $this->respondWithValidationError($validator->messages(),422);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            //Check if the email exist
+            if($user){
+                $checkIfPasswordMatch = Hash::check($request->password, $user->password, []);
+                if (!$checkIfPasswordMatch) {
+                    return $this->respondWithError("Email or Password does not match our record",403);
+                }
+                else {
+                    $tokenResult = $user->createToken('UserAuthToken')->plainTextToken;
+
+                    $data = [
+                        'token' =>  $tokenResult,
+                        'token_type' => 'Bearer'
+                    ];
+
+                    return $this->respondWithSuccess("User Logged in Successfully", 200, $data);
+                }
+
+            }else{
+                return $this->respondWithError("Email or Password is Incorrect", 403);
+            }
+
+        } catch (\Exception $error) {
+            return $this->exceptionError($error->getMessage(), 500);
+        }
+    }
+
+    public function validateUserRegistration(){
+        return Validator::make(request()->all(),[
+            'username' => 'required',
+            'email' => 'required|email|string|max:100|unique:users',
+            'password' => 'required|min:8',
+        ]);
+    }
+
+    public function validateUserLogin(){
+        return Validator::make(request()->all(),[
+            'email' => 'required',
+            'password' => 'required'
+        ]);
     }
 }
---
+++ app/Models/VerificationCode.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VerificationCode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id' , 'administrator_id', 'code', 'purpose', 'expires_at'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
